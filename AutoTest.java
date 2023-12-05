b055bd60d311e5246e40133bfd400290287bd185
Hcube32 <Yzhniysam@hotmail.com>
2023-12-05 20:12:02 +0300

    Third update from 05.12.23

---
+++ AutoTest.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous(name="Что нибудь", group="")
+public class AutoTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Robot2023 R = new Robot2023();
+        R.initFields(telemetry, this, hardwareMap);
+        R.init();
+        waitForStart();
+
+        //okay, let's go!
+
+    }
+
+}
